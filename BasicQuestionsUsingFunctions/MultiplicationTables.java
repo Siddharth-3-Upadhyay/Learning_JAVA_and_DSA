4731308ff10518f1683362f7dc3042c4e385df61
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-27 14:19:21 +0530

    Multiplication Table code.

---
+++ BasicQuestionsUsingFunctions/MultiplicationTables.java
@@ -0,0 +1,21 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class MultiplicationTables {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        System.out.print("Enter the num: ");
+        num = input.nextInt();
+        multiplicationTables(num);
+
+        input.close();
+    }
+    static void multiplicationTables(int num) {
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}

4d9cb3e87e5814518c45f8109b72fe38c2319a50
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-27 20:31:28 +0530

    Area of Parallelogram program.

--- BasicQuestionsUsingFunctions/Area.java
+++ BasicQuestionsUsingFunctions/Area.java
@@ -8,7 +8,8 @@ public class Area {
     public static void main(String[] args) {
 //          areaOfCircle();
 //        areaOfTriangle();
-        areaOfRectangle();
+//        areaOfRectangle();
+        areaOfParallelogram();
     }
     static void areaOfCircle() {
         /* Area of Circle = PI * r * r */
@@ -16,7 +17,7 @@ public class Area {
         System.out.print("Enter the radius of Circle(m): ");
         radius = input.nextFloat();
         float area = PI * radius * radius;
-        System.out.println("Area of Circle: " + area);
+        System.out.println("Area of Circle: " + area + " sq.m");
     }
     static void areaOfTriangle() {
         /* Area of Triangle = 1.5 * base * height */
@@ -25,7 +26,7 @@ public class Area {
         base = input.nextFloat();
         height = input.nextFloat();
         float area = 0.5F * base * height;
-        System.out.println("Area of Triangle: " + area);
+        System.out.println("Area of Triangle: " + area + " sq.m");
     }
     static void areaOfRectangle() {
         /* Area of Rectangle = length * breadth */
@@ -34,6 +35,21 @@ public class Area {
         length = input.nextFloat();
         breadth = input.nextFloat();
         area = length * breadth;
-        System.out.println("Area of Rectangle: " + area);
+        System.out.println("Area of Rectangle: " + area + " sq.m");
+    }
+    static void areaOfParallelogram() {
+        /* 1. When base and height of parallelogram are given.
+        *       Area = base * height
+        * 2. When height is not given
+        *       Area = a * b * sin(x)
+        *       where a, b are the length of parallel sides.
+        *           and x is the angle between them.*/
+        // for the simplicity we will go with 1st case.
+        float base, height, area;
+        System.out.print("Enter the base(m) and height(m) of parallelogram: ");
+        base = input.nextFloat();
+        height = input.nextFloat();
+        area = base * height;
+        System.out.println("Area of Parallelogram: " + area + " sq.m");
     }
 }

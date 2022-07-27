cd87f4ad1f3de586157a22d0f514b928aa64fb7c
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-27 14:42:38 +0530

    Fibonacci Series program.

---
+++ BasicQuestionsUsingFunctions/FibonacciSeries.java
@@ -0,0 +1,32 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int lastNum;
+        System.out.print("Enter the last number upto which you want your fibonacci series: ");
+        lastNum = input.nextInt();
+        fiboNum(lastNum);
+
+        input.close();
+    }
+    static void fiboNum(int lastNum) {
+        int firstNum = 0;
+        int secondNum = 1;
+        int nextNum;
+        System.out.print(firstNum + ", ");
+        System.out.print(secondNum + ", ");
+        for (int i = 3; i <= lastNum; i++) {
+            nextNum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = nextNum;
+            System.out.print(nextNum);
+            if (i != lastNum)
+                System.out.print(", ");
+        }
+        System.out.println();
+    }
+}

88cd0127d31d22aa5bb88de23a820be45bd01bee
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-27 14:36:48 +0530

    Currency Convertor program.

---
+++ BasicQuestionsUsingFunctions/CurrencyConvertor.java
@@ -0,0 +1,21 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class CurrencyConvertor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int rupee;
+        System.out.print("Enter Currency(₹): ");
+        rupee = input.nextInt();
+        rupeeToUSD(rupee);
+
+        input.close();
+    }
+    static void rupeeToUSD(int rupee) {
+        // currently(27 July 2022): Rs 80 = $1
+        float usd = rupee * 0.01F;
+        System.out.println("₹" + rupee + " is $" + usd);
+    }
+}

c785b8c0e57b10d1ee27418ff9fc7c9a738fccb4
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-27 14:23:36 +0530

    Even or Odd program.

---
+++ BasicQuestionsUsingFunctions/EvenOrOdd.java
@@ -0,0 +1,22 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+        evenOdd(num);
+
+        input.close();
+    }
+    static void evenOdd (int num) {
+        if (num % 2 == 0)
+            System.out.println(num + " is Even.");
+        else
+            System.out.println(num + " is Odd.");
+    }
+}

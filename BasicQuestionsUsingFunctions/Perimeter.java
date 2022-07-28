b9ed44b8199b2e6e98b4b67942de995a4c0cf3f0
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-28 10:54:12 +0530

    Perimeter of Parallelogram program. And correct units in existing program.

--- BasicQuestionsUsingFunctions/Perimeter.java
+++ BasicQuestionsUsingFunctions/Perimeter.java
@@ -7,7 +7,8 @@ public class Perimeter {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 //        perimeterOfCircle();
-        perimeterOfEquilateralTriangle();
+//        perimeterOfEquilateralTriangle();
+        perimeterOfParallelogram();
     }
     static void perimeterOfCircle() {
         /* Perimeter of Circle = 2 * PI * radius */
@@ -15,7 +16,7 @@ public class Perimeter {
         System.out.print("Enter the radius of circle: ");
         radius = input.nextFloat();
         perimeter = 2 * PI * radius;
-        System.out.println("Perimeter of Circle(m): " + perimeter);
+        System.out.println("Perimeter of Circle(m): " + perimeter + " m");
     }
     static void perimeterOfEquilateralTriangle() {
         /* Perimeter of Equilateral Triangle = 3 * side */
@@ -23,6 +24,15 @@ public class Perimeter {
         System.out.print("Enter the side of equilateral triangle: ");
         side = input.nextFloat();
         perimeter = 3 * side;
-        System.out.println("Perimeter of equi. triangle: " + perimeter);
+        System.out.println("Perimeter of equi. triangle: " + perimeter + " m");
+    }
+    static void perimeterOfParallelogram() {
+        /* Perimeter of Parallelogram = 2 * (a + b) */
+        float length, breadth, perimeter;
+        System.out.print("Enter the length(m) and breadth(m) of Parallelogram: ");
+        length = input.nextFloat();
+        breadth = input.nextFloat();
+        perimeter = 2 * (length + breadth);
+        System.out.println("Perimeter of Parallelogram: " + perimeter + " m");
     }
 }

731f6c5507fe99d4739b0e88f7d949b18330ce71
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-28 11:07:51 +0530

    Perimeter of Rectangle, Sqaure and Rhombus programs.

--- BasicQuestionsUsingFunctions/Perimeter.java
+++ BasicQuestionsUsingFunctions/Perimeter.java
@@ -6,14 +6,17 @@ public class Perimeter {
     static final float PI = 3.14159F;
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-//        perimeterOfCircle();
-//        perimeterOfEquilateralTriangle();
+        perimeterOfCircle();
+        perimeterOfEquilateralTriangle();
         perimeterOfParallelogram();
+        perimeterOfRectangle();
+        perimeterOfSqaure();
+        perimeterOfRhombus();
     }
     static void perimeterOfCircle() {
         /* Perimeter of Circle = 2 * PI * radius */
         float radius, perimeter;
-        System.out.print("Enter the radius of circle: ");
+        System.out.print("Enter the radius of Circle: ");
         radius = input.nextFloat();
         perimeter = 2 * PI * radius;
         System.out.println("Perimeter of Circle(m): " + perimeter + " m");
@@ -21,10 +24,10 @@ public class Perimeter {
     static void perimeterOfEquilateralTriangle() {
         /* Perimeter of Equilateral Triangle = 3 * side */
         float side, perimeter;
-        System.out.print("Enter the side of equilateral triangle: ");
+        System.out.print("Enter the side of Equilateral Triangle: ");
         side = input.nextFloat();
         perimeter = 3 * side;
-        System.out.println("Perimeter of equi. triangle: " + perimeter + " m");
+        System.out.println("Perimeter of Equilateral Triangle: " + perimeter + " m");
     }
     static void perimeterOfParallelogram() {
         /* Perimeter of Parallelogram = 2 * (a + b) */
@@ -34,5 +37,30 @@ public class Perimeter {
         breadth = input.nextFloat();
         perimeter = 2 * (length + breadth);
         System.out.println("Perimeter of Parallelogram: " + perimeter + " m");
+    }
+    static void perimeterOfRectangle() {
+        /* Perimeter of Rectangle = 2 * (l + b) */
+        float length, breadth, perimeter;
+        System.out.print("Enter the length(m) and breadth(m) of Rectangle: ");
+        length = input.nextFloat();
+        breadth = input.nextFloat();
+        perimeter = 2 * (length + breadth);
+        System.out.println("Perimeter of Rectangle: " + perimeter + " m");
+    }
+    static void perimeterOfSqaure() {
+        /* Perimeter of Sqaure = 4 * length */
+        float length, perimeter;
+        System.out.print("Enter the length(m) of Sqaure: ");
+        length = input.nextFloat();
+        perimeter = 4 * length;
+        System.out.println("Perimeter of Sqaure: " + perimeter + " m");
+    }
+    static void perimeterOfRhombus() {
+        /* Perimeter of Rhombus = 4 * length */
+        float length, perimeter;
+        System.out.print("Enter the length(m) of Rhombus: ");
+        length = input.nextFloat();
+        perimeter = 4 * length;
+        System.out.println("Perimeter of Rhombus: " + perimeter + " m");
     }
 }

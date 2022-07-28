bd14430a575c044a94d88b4a26f277114b41698a
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-28 12:04:02 +0530

    Volume of Cylinder program.

--- BasicQuestionsUsingFunctions/Volume.java
+++ BasicQuestionsUsingFunctions/Volume.java
@@ -6,7 +6,8 @@ public class Volume {
     static final float PI = 3.14159F;
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        volumeOfCone();
+//        volumeOfCone();
+        volumeOfCylinder();
     }
     static void volumeOfCone() {
         /* Volume of Cone = (PI * radius * radius * height) / 3 */
@@ -16,5 +17,14 @@ public class Volume {
         height = input.nextFloat();
         volume = (PI * (radius * radius) * height) / 3;
         System.out.println("Volume of Cone: " + volume + " cubic m");
+    }
+    static void volumeOfCylinder() {
+        /* Volume of Cylinder = PI * radius * radius * height */
+        float radius, height, volume;
+        System.out.print("Enter radius(m) and height(m) of Cylinder: ");
+        radius = input.nextFloat();
+        height = input.nextFloat();
+        volume = PI * (radius * radius) * height;
+        System.out.println("Volume of Cylinder: " + volume + " cubic m");
     }
 }

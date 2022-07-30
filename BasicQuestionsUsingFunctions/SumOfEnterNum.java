207132889ca3a2e91b77f63b9f3715f6feabf937
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-30 11:44:43 +0530

    Sum of all the numbers entered by the user.

---
+++ BasicQuestionsUsingFunctions/SumOfEnterNum.java
@@ -0,0 +1,19 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class SumOfEnterNum {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        sumOfEnterNum();
+    }
+    static void sumOfEnterNum() {
+        float num, sum = 0;
+        do {
+            System.out.print("Enter number, press '0' to exit: ");
+            num = input.nextFloat();
+            sum += num;
+        } while (num != 0);
+        System.out.println("Sum: " + sum);
+    }
+}

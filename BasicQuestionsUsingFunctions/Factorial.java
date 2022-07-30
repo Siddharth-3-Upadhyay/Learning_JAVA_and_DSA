9ee8439951de21d7183c7e62677ee78897fcba5c
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-30 11:59:27 +0530

    Factorial of a number.

---
+++ BasicQuestionsUsingFunctions/Factorial.java
@@ -0,0 +1,20 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num;
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+        factorialNum(num);
+    }
+    static void factorialNum(int num) {
+        int fact = 1;
+        for (int i = num; i > 1; i--) {
+            fact *= i;
+        }
+        System.out.println("Factorial: " + fact);
+    }
+}

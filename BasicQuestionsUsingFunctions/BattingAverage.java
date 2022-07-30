36d2061e0bfd3bb2cb0b1716bd2e8100454ea5a3
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-30 13:21:13 +0530

    Batting average in cricket.

---
+++ BasicQuestionsUsingFunctions/BattingAverage.java
@@ -0,0 +1,24 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class BattingAverage {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Batting Average: " + battingAverage());
+    }
+    static double battingAverage() {
+        float runs, innings, notOut;
+        System.out.print("Enter the number of innings batted: ");
+        innings = input.nextFloat();
+        System.out.print("Enter the number of times not-out: ");
+        notOut = input.nextFloat();
+        System.out.print("Enter runs scored: ");
+        runs = input.nextFloat();
+
+        if (innings == notOut)
+            return runs;
+        else
+            return runs / (innings - notOut);
+    }
+}

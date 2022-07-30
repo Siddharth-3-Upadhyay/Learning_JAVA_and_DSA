8b35af95ff23c076129fcfe269e2964229c6d918
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-30 12:45:28 +0530

    Depreciation Value of a product.

---
+++ BasicQuestionsUsingFunctions/DepreciationValue.java
@@ -0,0 +1,21 @@
+package BasicQuestionsUsingFunctions;
+
+import java.util.Scanner;
+
+public class DepreciationValue {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        float v1, r, t;
+        System.out.print("Enter Value, Rate and Time: ");
+        v1 = input.nextFloat();
+        r = input.nextFloat();
+        t = input.nextFloat();
+        System.out.println("Depreciated Value: " + depreciation(v1, r, t));
+    }
+    static float depreciation(float v1, float r, float t) {
+        /* Depreciation value = v1 * Math.pow((1 - r / 100), t) */
+        float depValue;
+        depValue = (float)(v1 * Math.pow((1 - r / 100), t));
+        return depValue;
+    }
+}

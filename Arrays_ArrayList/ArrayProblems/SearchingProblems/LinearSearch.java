2baf2268f2d332e20fc36189024d5676b2c0d20c
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-07-31 16:38:33 +0530

    Minimum element in array using LinearSearch.

--- Arrays_ArrayList/ArrayProblems/SearchingProblems/LinearSearch.java
+++ Arrays_ArrayList/ArrayProblems/SearchingProblems/LinearSearch.java
@@ -5,6 +5,7 @@ public class LinearSearch {
         int[] arr = {18, 12, -7, 3, 14, 28};
         int target = 14;
         System.out.println(searchInRange(arr, target, 1, 4));
+        System.out.println(minInArray(arr));
     }
     static int searchInRange(int[] arr, int target, int start, int end) {
         /* return the index if found, else return -1 */
@@ -12,5 +13,12 @@ public class LinearSearch {
             if (arr[i] == target) return i;
         }
         return -1;
+    }
+    static int minInArray(int[] arr) {
+        int minElement = arr[0]; // 18.
+        for (int i : arr) {
+            if (i < minElement) minElement = i;
+        }
+        return minElement;
     }
 }

267a0abd844bc38486d4cca5823bbb544d13bad4
Siddharth Upadhyay <siddharth.3.upadhyay@proton.me>
2022-08-01 14:22:59 +0530

    Ceiling of a number in an sorted array (Binary Search).

---
+++ Arrays_ArrayList/ArrayProblems/SearchingProblems/CeilingInArray.java
@@ -0,0 +1,33 @@
+package Arrays_ArrayList.ArrayProblems.SearchingProblems;
+
+public class CeilingInArray {
+    public static void main(String[] args) {
+        int[] arrAsc = {-18, -12, -4, 0, 2, 3, 5, 15, 16, 18, 22, 45, 89};
+        int[] arrDes = {88, 79, 62, 46, 20, 15, 7, 5, 1, 0, - 8, -12, -29};
+        int target = 15;
+        System.out.println(ceilingInArray(arrAsc, target));
+        System.out.println(ceilingInArray(arrDes, target));
+    }
+    /*
+    Ceiling of a number is the smallest number greater or equal to target number.
+    i.e., ceilingNumb >= target 
+     */
+    static int ceilingInArray(int[] numbs, int target) {
+        int startIndex = 0;
+        int endIndex = numbs.length - 1;
+        boolean isAscending = numbs[startIndex] < numbs[endIndex];
+        while (startIndex <= endIndex) {
+            int midIndex = startIndex + (endIndex - startIndex) / 2;
+            if (target == numbs[midIndex]) return midIndex;
+            if (target < numbs[midIndex]) {
+                if (isAscending) endIndex = midIndex - 1;
+                else startIndex = midIndex + 1;
+            }
+            else {
+                if (isAscending) startIndex = midIndex + 1;
+                else endIndex = midIndex - 1;
+            }
+        }
+        return startIndex;
+    }
+}
